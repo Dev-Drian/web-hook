5aefd3afaa1ca6d1dd552cb29c6886c45239c21e
DevDrian <matiose12@gmail.com>
2025-01-04 01:38:00 -0500

    ya sirve sol o toca poner el enpoint y ya

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,21 +11,16 @@ class WebhookController extends Controller
     public function recibirReserva(Request $request)
     {
         $datos = [
-            [
-                "first_name" => "River",
-                "last_name"  => "Green",
-                "age"        => 27
-            ],
-            [
-                "first_name" => "Alex",
-                "last_name"  => "Smith",
-                "age"        => 67
-            ],
-           
+            'nombre' => 'adrian castro',          // Nombre del usuario
+            'email' => 'juan.perez@example.com', // Correo electrónico
+            'fecha_reserva' => '2025-01-15',     // Fecha de la reserva (formato YYYY-MM-DD)
+            'hora_reserva' => '14:30',           // Hora de la reserva (formato HH:MM)
+            'descripcion' => 'Reserva para cena', // Descripción del evento
+            'ubicacion' => 'Restaurante ABC',    // Ubicación del evento
         ];
 
         // URL del webhook de Zapier
-        $urlWebhook = 'https://hooks.zapier.com/hooks/catch/21160374/28qcxvo/';
+        $urlWebhook = 'https://hooks.zapier.com/hooks/catch/21200123/2zys123/';
 
         // Enviar datos con POST
         $response = Http::post($urlWebhook, $datos);
